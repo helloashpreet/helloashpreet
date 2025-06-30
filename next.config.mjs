f511d70aade25449ecc3d503957c7a82360f3b93
helloashpreet <94135039+helloashpreet@users.noreply.github.com>
2025-07-01 00:37:55 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/notes',
-        permanent: false,
-      },
-      {
-        source: '/:path((?!notes|api|messages|_next|static|public|favicon\\.ico|sitemap\\.xml|robots\\.txt).*)',
-        destination: '/notes/:path',
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
